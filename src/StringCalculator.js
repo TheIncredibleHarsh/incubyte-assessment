fa5faa952491efc9821e00cbbc9f8f37495813bc
Harsh Kumar <harsh.singh343@gmail.com>
2024-07-16 00:23:02 +0530

    Test for straight forward cases

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -2,7 +2,7 @@ export const addString = (inputString) => {
     let sum = 0;
 
     inputString.split(",").map(number => {
-        sum+=parseInt(number)        
+        sum+=parseInt(number || 0)        
     })
     return sum;
 }
--- test/StringCalculatorTest.js
+++ test/StringCalculatorTest.js
@@ -6,5 +6,16 @@ describe("StringCalculator", () => {
         it("should return 0 for empty string", () => {
             assert.equal(addString(""), 0);
         })
+
+        it("should return the value if the string has only 1 number", () => {
+            const num = "5";
+            assert.equal(addString(num), parseInt(num));
+        })
+
+        it("should add comma separated numbers", () => {
+            assert.equal(addString("1,4,7"), 12);
+            assert.equal(addString("5,8"), 13);
+            assert.equal(addString("34,52,87,95,62,87"), 417);
+        })
     })
 })
